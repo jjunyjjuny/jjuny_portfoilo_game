7aa03b0c50982c5c945557b22045bc3146680807
jjunyjjuny <jjuny546@gmail.com>
2020-09-28 19:20:29 +0900

    a1

--- src/index.js
+++ src/index.js
@@ -1,32 +1,22 @@
-import Phaser from 'phaser';
-import MainScene from './scenes/mainScene';
-import MainMenuScene from './scenes/mainMenuScene';
-import GameOverScene from './scenes/gameOverScene';
-import LeaderboardScene from './scenes/leaderboardScene';
-import InstructionsScene from './scenes/instructionsScene';
+import Phaser from "phaser";
+import MainMenuScene from "./scenes/mainMenuScene";
 
 const config = {
   type: Phaser.AUTO,
   width: window.innerWidth,
   height: window.innerHeight - 5,
-  parent: 'divId',
+  parent: "divId",
   dom: {
     createContainer: true,
   },
   physics: {
-    default: 'arcade',
+    default: "arcade",
     arcade: {
       gravity: { y: 300 },
       debug: false,
     },
   },
-  scene: [
-    MainMenuScene,
-    MainScene,
-    InstructionsScene,
-    GameOverScene,
-    LeaderboardScene,
-  ],
+  scene: [MainMenuScene],
 };
 
 const game = new Phaser.Game(config); // eslint-disable-line no-unused-vars
